fa7de17b93e52749e1175040becc47e3a6b1eea1
Emil Sharier <emil.sharier.dev@gmail.com>
2025-02-22 17:56:30 +0530

    added index to blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,21 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
     markdown: {
-        remarkPlugins: [remarkReadingTime]
+        remarkPlugins: [remarkReadingTime],
+        rehypePlugins: [rehypeHeadingIds]
     },
     integrations: [
-        mdx(),
+        mdx({
+            rehypePlugins: [rehypeHeadingIds],
+            remarkPlugins: [remarkReadingTime]
+            // extendMarkdownConfig: true
+        }),
         sitemap(),
         tailwind({
             applyBaseStyles: false
